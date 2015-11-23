7d79f744e1fe5df150244eeb5240e26740d7e187
gopalagrawal75 <gopal@jlabs.co>
2015-11-23 14:39:52 +0530

    random

--- insert.py
+++ insert.py
@@ -2,15 +2,18 @@ from bson.json_util import dumps
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import re
+import json
 from . import db
 
 failure = dumps({"Failed"})
 @csrf_exempt
 def insert_query(request):
     data = db.test
+    #res =json.loads(request)
+   # print(request)
     try:
-        result=data.insert(request)
-        return HttpResponse("{'success':1}", content_type="application/json")
+        result=data.insert({'success': 1})
+        return HttpResponse("{success:1}", content_type="application/json")
     except:
         return HttpResponse(failure, content_type="application/json")
     return HttpResponse(failure, content_type="application/json")
