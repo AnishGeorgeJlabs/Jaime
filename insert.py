d316b26562c921928bcc09b3bd943239ef0636ec
gopalagrawal75 <gopal@jlabs.co>
2015-11-23 14:54:46 +0530

    random

--- insert.py
+++ insert.py
@@ -9,11 +9,9 @@ failure = dumps({"Failed"})
 @csrf_exempt
 def insert_query(request):
     data = db.test
-    #res =json.loads(request)
-   # print(request)
     try:
-        result=data.insert({'success': 1})
-        return HttpResponse("{success:1}", content_type="application/json")
+        result=data.insert(request.body)
+        return HttpResponse(request)
     except:
         return HttpResponse(failure, content_type="application/json")
     return HttpResponse(failure, content_type="application/json")
