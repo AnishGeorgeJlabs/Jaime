0c4af2710c6aabae95ebc1bc8af64f0fa278d4c0
gopalagrawal75 <gopal@jlabs.co>
2015-11-23 13:14:40 +0530

    random

--- __init__.py
+++ __init__.py
@@ -4,5 +4,5 @@ import pymongo
 dbclient = pymongo.MongoClient("45.55.232.5:27017")
 dbclient.jaime.authenticate('jaimeApi', 'goldenHead', mechanism='MONGODB-CR')
 
-# hellow
+# hello
 db = dbclient.jaime
