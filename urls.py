cb6a4a2605eeda408aaccfc06d4a21e1c150e22f
pradeep9868 <prakumk@gmail.com>
2016-01-27 12:42:49 +0530

    New Update

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
-from . import search,search_location,insert,gmap,scrap,datascrap,description,all_reviews,add_review
+from . import search,search_location,insert,gmap,scrap,description,all_reviews,add_review
 import json
 
 @csrf_exempt
@@ -26,7 +26,6 @@ urlpatterns = [
     url(r'^$', test),
     url(r'^search',  search.search),
     url(r'^scrap',  scrap.scrap),
-    url(r'^datascrap',  datascrap.datascrap),
     url(r'^description',  description.description),
     url(r'^reviews',  all_reviews.all_reviews),
     url(r'^add_review',  add_review.add_review),
