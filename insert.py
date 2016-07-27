39b8cf910bb6e49b6f7f73fc5a148b548b2f9b60
Gopal <gopal@jlabs.co>
2016-07-27 12:28:53 +0530

    fix identation issue

--- insert.py
+++ insert.py
@@ -7,34 +7,33 @@ import re
 failure = dumps({"Failed"})
 @csrf_exempt
 def insert_query(request):
-    try:
-        data = get_json(request)
-        collection =db.fe_app
-        gmapinfo = db.gmapinfo
-        fe_track=db.fe_track
-        try:
-            result=collection.distinct("uniq_id")
-            data['uniq_id']=max(result)+1
-        except:
-            data['uniq_id']= 1
-        data['review']=0
-        data['confirm']=0
-        data['rating']=3.5
-        if "uniq_code" in data:
-            gmapinfo.delete_one({"uniq_id": data['uniq_code']})
-            del data['uniq_code']
-        collection.insert(data)
-
-        new_dict = dict()
+	try:
+		data = get_json(request)
+		collection =db.fe_app
+		gmapinfo = db.gmapinfo
+		fe_track=db.fe_track
+		try:
+			result=collection.distinct("uniq_id")
+			data['uniq_id']=max(result)+1
+		except:
+			data['uniq_id']= 1
+		data['review']=0
+		data['confirm']=0
+		data['rating']=3.5
+		if "uniq_code" in data:
+			gmapinfo.delete_one({"uniq_id": data['uniq_code']})
+			del data['uniq_code']
+		collection.insert(data)
+		new_dict = dict()
 		new_dict['uniq_id'] = data['uniq_id']
-        new_dict['loc']=data['loc']
-        now = datetime.now() + timedelta(hours=5,minutes=30)
-        new_dict['date_time'] = now.strftime("%Y-%m-%d %H:%M")
-        fe_track.insert(new_dict)
+		new_dict['loc']=data['loc']
+		now = datetime.now() + timedelta(hours=5,minutes=30)
+		new_dict['date_time'] = now.strftime("%Y-%m-%d %H:%M")
+		fe_track.insert(new_dict)
 
-        return basic_success(data)
-    except Exception as e:
-        return basic_error(e)
+		return basic_success(data)
+	except Exception as e:
+		return basic_error(e)
 
 def fe_track(request):
 	try:
